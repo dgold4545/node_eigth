8d7dca6dd8b07801e133fbb1b941a8f93071ef5a
DGOLD7788 <80936904545dgold@gmail.com>
2024-05-29 16:44:02 +0300

    d

--- node-tut/index.js
+++ node-tut/index.js
@@ -1,3 +1,21 @@
 const message = 'NodeJS is amazing!'
 
 console.log(message);
+
+const validator = require("validator");
+
+const validateEmail = (email) => {
+  return validator.isEmail(email);
+};
+
+console.log(
+  "Is mango@mail.com a valid email?: ",
+  validateEmail("mango@mail.com")
+);
+
+console.log(
+  "Is Mangozedog.com a valid email?: ",
+  validateEmail("Mangozedog.com")
+);
+
+
